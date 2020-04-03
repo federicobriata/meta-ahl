db72849202e6a3c378f1131fc9945c4a5f27474e
Federico Briata <federicobriata@gmail.com>
2020-04-03 17:34:46 +0200

    adding jack commands to better understand how we route jack connections

--- recipes-multimedia/foxdot/files/start_foxdot.sh
+++ recipes-multimedia/foxdot/files/start_foxdot.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 sclang /usr/share/SuperCollider/Extensions/FoxDot/foxdot.scd &
+# SC connect to jack by default
 #jack_connect "SuperCollider:out_1" "system:playback_1"
 #jack_connect "SuperCollider:out_2" "system:playback_2"
+
+# Uncomment to route SC to Ninjam
+#jack_diconnect "SuperCollider:out_1" "system:playback_1"
+#jack_diconnect "SuperCollider:out_2" "system:playback_2"
+#jack_connect "SuperCollider:out_1" "ninjam:in1"
+#jack_connect "SuperCollider:out_2" "ninjam:in2"
+
 sleep 3
 foxdot.py
 
--- recipes-multimedia/ninjam-client/files/start_ninjam.sh
+++ recipes-multimedia/ninjam-client/files/start_ninjam.sh
@@ -1,7 +1,8 @@
 pkill cninjam
 pkill ninjamsrv
 ninjamsrv /etc/ninjam.cfg &
-cninjam 127.0.0.1 -user anonymous:aidadsp -jack &
+cninjam 127.0.0.1 -user anonymous:aidadsp -jack
+# cninjam don't work in background, atm jack_connect need to run manually.
 #jack_connect "ninjam:out1" "system:playback_1"
 #jack_connect "ninjam:out2" "system:playback_2"
 
